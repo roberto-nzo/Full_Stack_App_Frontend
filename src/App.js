a36aa2512874cb212396e1d201de1fe1e264d7c2
Roberto Nzohabonayo <robertonzohabonayo@gmail.com>
2023-03-21 14:12:54 +0200

    menu with conditions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import 'react-toastify/dist/ReactToastify.css'
 import Navs from "./components/Navs";
 import Home from "./pages/home"
 import Students from './pages/students/students'
-import AddStudent from './pages/students/addstudent';
 import Courses from './pages/courses/courses'
 import Addcourse from './pages/courses/addcourse';
 import Classes from './pages/classes/classes'
@@ -13,77 +12,27 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 
 function App() {
-  const menuBtn = document.querySelector('.menu-btn')
-  const sideBar = document.querySelector('.side-bar')
-  const options = document.querySelector('.options')
-
-  let showMenu = false
-
-  function toggleMenu() {
-    if (!showMenu) {
-      menuBtn.classList.add('close')
-      sideBar.classList.add('show')
-      options.classList.add('show')
-
-      showMenu = true
-    } else {
-      menuBtn.classList.remove('close')
-      sideBar.classList.remove('show')
-      options.classList.remove('show')
-
-      showMenu = false
-    }
-  }
 
   return (
     <>
       <div className="container">
 
-        <div className='menu-btn' onClick={toggleMenu}>
-          <div className="menu-bar"></div>
-          <div className="menu-bar"></div>
-          <div className="menu-bar"></div>
-        </div>
+        <Navs>
 
-        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/students" element={<Students />} />
+            {/* <Route path="/addstudent" element={<AddStudent />} /> */}
+            <Route path="/classes" element={<Classes />} />
+            <Route path="/addclass" element={<Addclass />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/addcourse" element={<Addcourse />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+          <ToastContainer />
 
-          <div className="side-bar">
-
-            <div className="profile">
-              <div className="profile-branding">
-                <a href="/"><div className="portrait">
-
-                </div></a>
-              </div>
-              {/* <div className="description">
-                <div className="name-id">
-                  <p>Roberto Nzohabonayo</p>
-                  <p>ID: 20185281</p>
-                </div>
-                <div className="view-profile">
-                  <div className="view"><a href="">View Profile</a></div>
-                </div>
-              </div> */}
-            </div>
-
-            <Navs />
-          </div>
-          <div className="options">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/students" element={<Students />} />
-              {/* <Route path="/addstudent" element={<AddStudent />} /> */}
-              <Route path="/classes" element={<Classes />} />
-              <Route path="/addclass" element={<Addclass />} />
-              <Route path="/courses" element={<Courses />} />
-              <Route path="/addcourse" element={<Addcourse />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/login' element={<Login />} />
-            </Routes>
-            <ToastContainer />
-          </div>
-
-        </div>
+        </Navs>
       </div>
     </>
   );
--- src/components/Navs.jsx
+++ src/components/Navs.jsx
@@ -1,22 +1,53 @@
 import { Link } from 'react-router-dom'
 import { BsPeopleFill } from "react-icons/bs";
 import { SiGoogleclassroom, SiBookstack } from "react-icons/si";
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
-function Navs() {
+function Navs({ children }) {
+
+    const [navOpen, setNavOpen] = useState(false)
+    const { user } = useSelector((state) => state.auth)
+
     return (
-        <div className='side-links'>
-            <div className='side-link'>
-                <Link to='/students' className='_link'><BsPeopleFill /> Students</Link>
+        <>
+
+            <div className={'menu-btn' + (navOpen && user ? ' close' : '')} onClick={() => { setNavOpen(!navOpen) }}>
+                <div className="menu-bar"></div>
+                <div className="menu-bar"></div>
+                <div className="menu-bar"></div>
             </div>
 
-            <div className='side-link'>
-                <Link to='/classes' className='_link'><SiGoogleclassroom /> Classes</Link>
+            <div className="content">
+                <div className={"side-bar " + (navOpen && user ? 'show' : '')}>
+                    <div className="profile">
+                        <div className="profile-branding">
+                            <a href="/">
+                                <div className="portrait">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div className='side-links'>
+                        <div className='side-link'>
+                            <Link to='/students' className='_link'><BsPeopleFill /> Students</Link>
+                        </div>
+
+                        <div className='side-link'>
+                            <Link to='/classes' className='_link'><SiGoogleclassroom /> Classes</Link>
+                        </div>
+
+                        <div className='side-link'>
+                            <Link to='/courses' className='_link'><SiBookstack /> Courses</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div className='side-link'>
-                <Link to='/courses' className='_link'><SiBookstack /> Courses</Link>
+            <div className={"options " + (navOpen && user ? 'show' : '')}>
+                {children}
             </div>
-        </div>
+        </>
     )
 }
 
